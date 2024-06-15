aecc4702aba9bf3b5e5ac8b8edf189b501a68988
Valentine Asogwa <vasogwaze@gmail.com>
2024-06-15 14:02:10 +0100

    element setup

---
+++ App/Components/Elements/RootContainer.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function RootContainer() {
+  return (
+    <View>
+      <Text>RootContainer</Text>
+    </View>
+  );
+}
---
+++ App/Components/Elements/index.ts
@@ -0,0 +1,3 @@
+export {default as RootContainer} from './RootContainer';
+export {default as Container} from './Container';
+export {default as Text} from './Text';
