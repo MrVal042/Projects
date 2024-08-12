39d73649a74a80e746bfa69f8f87572c4c31bfcc
Valentine Asogwa <vasogwaze@gmail.com>
2024-08-12 15:42:50 +0100

    Added google and google photos

--- App/screen/Projects/Google.tsx
+++ App/screen/Projects/Google.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import {ComingSoon, RootContainer} from '@component';
+import {RootContainer} from '@component';
+import WebView from 'react-native-webview';
 
 export default function Google() {
   return (
     <RootContainer title="Google">
-      <ComingSoon title="Google" />
+      <WebView
+        source={{uri: 'https://www.google.com'}}
+        style={{flex: 1}}
+        showsVerticalScrollIndicator={false}
+      />
     </RootContainer>
   );
 }
--- App/screen/Projects/Photos.tsx
+++ App/screen/Projects/Photos.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import {RootContainer, ComingSoon} from '@component';
+import {RootContainer} from '@component';
+import WebView from 'react-native-webview';
 
 export default function Photos() {
   return (
     <RootContainer title="Photos">
-      <ComingSoon title="Photos" />
+      <WebView
+        source={{uri: 'https://photos.google.com/'}}
+        style={{flex: 1}}
+        showsVerticalScrollIndicator={false}
+      />
     </RootContainer>
   );
 }
